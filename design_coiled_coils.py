8ce6c3b2c20a6341967ecdd69babe9393564c442
James MacDonald <jtmacdonald@gmail.com>
2018-12-10 18:41:17 +0000

    minor changes

--- design_coiled_coils.py
+++ design_coiled_coils.py
@@ -1,5 +1,21 @@
+#!/usr/bin/python
+
+import math, getopt, sys
 
 import simulated_annealing
+
+perm_res = [['I'],
+       ['K','H', 'S', 'T', 'Y', 'C', 'R', 'D', 'E'],
+       ['E', 'K','H', 'S', 'T', 'Y', 'C', 'R', 'D'],
+       ['L'],
+       ['E'],
+       ['H','S', 'T', 'Y', 'C', 'R', 'D', 'E', 'K'],
+       ['K']]
+
+
+seq = simulated_annealing.random_seq(perm_res)
+simulated_annealing.anneal(seq, simulated_annealing.score_func, simulated_annealing.point_mut, perm_res)
+
 
 
 
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -146,9 +146,9 @@ def accept_probability(old_score, new_score, T):
        else:
               return math.exp((old_score - new_score)/T)
 
-def anneal(sol):
-    original = copy.deepcopy(sol)
-    old_score = score_func(sol)
+def anneal(seq, score_func_, mut_func_, perm_res):
+    original = copy.deepcopy(seq)
+    old_score = score_func_(seq)
     T = 1.0
     T_min = 0.00001
     alpha = 0.99
@@ -156,26 +156,26 @@ def anneal(sol):
     scores = []
     iterations = []
     while T > T_min:
-        new_sol = point_mut(sol, l)
+        new_sol = mut_func_(seq, perm_res)
         #print(sol, new_sol)
-        new_score = score_func(new_sol)
+        new_score = score_func_(new_sol)
         ap = accept_probability(old_score, new_score, T)
         #print(ap, old_score, new_score, T)
         randnum = random.uniform(0,1)
         #print(ap, randnum)
         if ap > randnum:
             print("accepted")
-            sol = new_sol
+            seq = new_sol
             old_score = new_score
         else:
             print("rejected")
-        print(i, T, sol, score_func(sol)) # old_score)
-        scores.append(score_func(sol))
+        print(i, T, seq, score_func_(seq)) # old_score)
+        scores.append(score_func_(seq))
         iterations.append(i)
         T = T*alpha
         i +=1
-    np.savetxt('start_' + str(original) + '_end_' + str(sol) + '.dat', np.column_stack((iterations, scores)), fmt='%f')
-    return sol, score_func(sol)
+    np.savetxt('start_' + str(original) + '_end_' + str(seq) + '.dat', np.column_stack((iterations, scores)), fmt='%f')
+    return seq, score_func_(seq)
 
 
 
