1211b18d5ace91fc879c9e8c61af49c726b5a4c2
Karan Bali <karanbali@dyn3222-251.wlan.ic.ac.uk>
2018-12-11 17:38:04 +0000

    Font size changes

--- sa_graph.py
+++ sa_graph.py
@@ -12,13 +12,13 @@ start = sys.argv[1]
 sol = sys.argv[2]
 iterations, scores = loadtxt('start_' + str(start) + '_end_' + str(sol) + '.dat', unpack=True, usecols=[0,1])
 
-def movingaverage(values, window):
-    weights = np.repeat(1.0, window) / window
-    smas = np.convolve(values, weights, 'valid')
-    return smas
+#def movingaverage(values, window):
+    #weights = np.repeat(1.0, window) / window
+    #smas = np.convolve(values, weights, 'valid')
+    #return smas
 
-iterations = movingaverage(iterations, 10)
-scores = movingaverage(scores, 10)
+#iterations = movingaverage(iterations, 10)
+#scores = movingaverage(scores, 10)
 
 fig = plt.figure()
 
@@ -26,7 +26,7 @@ ax = fig.add_subplot(111)
 ax.plot(iterations,scores,'k-',lw=1,label=str(start)+'_'+str(sol))
 ax.set_xlabel(r'Iteration')
 ax.set_ylabel(r'Score')
-plt.title('start_'+str(start) + '_end_'+str(sol))
+plt.title('Start_'+str(start) + '_End_'+str(sol), fontsize=10)
 ax.figure.savefig('start_'+str(start)+'_end_'+str(sol)+'.pdf')
 plt.draw()
 
