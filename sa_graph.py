649898405f6db46b665a957751419654a35ee713
Karan Bali <karanbali@dyn3222-251.wlan.ic.ac.uk>
2018-12-11 17:22:02 +0000

    Included sys.argv so you can write arguments in command line

--- sa_graph.py
+++ sa_graph.py
@@ -4,28 +4,31 @@ import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import sys
 import matplotlib.ticker as ticker
 
-def sa_graph(start,sol):
-    iterations, scores = loadtxt('start_' + str(start) + '_end_' + str(sol) + '.dat', unpack=True, usecols=[0,1])
+#def sa_graph(start,sol):
+start = sys.argv[1]
+sol = sys.argv[2]
+iterations, scores = loadtxt('start_' + str(start) + '_end_' + str(sol) + '.dat', unpack=True, usecols=[0,1])
 
-    def movingaverage(values, window):
-        weights = np.repeat(1.0, window) / window
-        smas = np.convolve(values, weights, 'valid')
-        return smas
+def movingaverage(values, window):
+    weights = np.repeat(1.0, window) / window
+    smas = np.convolve(values, weights, 'valid')
+    return smas
 
-    iterations = movingaverage(iterations, 10)
-    scores = movingaverage(scores, 10)
+iterations = movingaverage(iterations, 10)
+scores = movingaverage(scores, 10)
 
-    fig = plt.figure()
+fig = plt.figure()
 
-    ax = fig.add_subplot(111)
-    ax.plot(iterations,scores,'k-',lw=1,label=str(start)+'_'+str(sol))
-    ax.set_xlabel(r'Iteration')
-    ax.set_ylabel(r'Score')
-    plt.title('start_'+str(start) + '_end_'+str(sol))
-    ax.figure.savefig('start_'+str(start)+'_end_'+str(sol)+'.pdf')
-    plt.draw()
+ax = fig.add_subplot(111)
+ax.plot(iterations,scores,'k-',lw=1,label=str(start)+'_'+str(sol))
+ax.set_xlabel(r'Iteration')
+ax.set_ylabel(r'Score')
+plt.title('start_'+str(start) + '_end_'+str(sol))
+ax.figure.savefig('start_'+str(start)+'_end_'+str(sol)+'.pdf')
+plt.draw()
 
 
 
